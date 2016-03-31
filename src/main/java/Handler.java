0e52de2ed54d7ecf007324d8dca66fe2b907e0ab
Robert Gu <robertgu@Roberts-MacBook-Pro.local>
2016-03-31 10:53:39 -0700

    Moved the responsibility of choosing which controller and which controller action to take to the router class
    Choose route in router class, chooses which controller, and controller
    action to take based of of the path and the httpverb.
    
    Delete htmltostring convertor since it is no longer being used

--- src/main/java/Handler.java
+++ src/main/java/Handler.java
@@ -22,54 +22,13 @@ public class Handler {
         request = Parser.parseRequest(socketInputStream);
         route = parseForRoute();
         boolean pathExists = router.pathExists(Parser.parseForPathUrl(getRoute()));
-        return handleResponse(pathExists);
-    }
-
-    private String handleResponse(boolean pathExists) throws IOException {
-        String response = "";
-        if(httpVerb(getRoute()).equals("GET") && path(getRoute()).equals("/")) {
-            String responseHeader = "HTTP/1.1 200 OK\r\n\r\n";
-            InputStream fileStream = Server.class.getResourceAsStream("index.html");
-            String responseBody = Parser.parseInputStream(fileStream);
-            response = responseHeader + responseBody;
-        } else if(path(getRoute()).equals("/form")) {
-            response = actionForForm();
-        } else if(httpVerb(getRoute()).equals("POST") && path(getRoute()).equals("/form")) {
-            response = "HTTP/1.1 200 OK\r\n";
-        } else if(httpVerb(getRoute()).equals("OPTIONS") && path(getRoute()).equals("/")) {
-            response = "HTTP/1.1 200 OK\r\nAllow: GET,HEAD,POST,OPTIONS,PUT\r\n";
-        } else if(pathExists) {
-            response = "HTTP/1.1 405 Method now allowed";
-        } else {
-            response = "HTTP/1.1 404 Not Found\r\n";
-        }
-
-        return response;
+        return router.chooseRoute(pathExists, route, request);
     }
 
     private String parseForRoute() throws IOException {
         String httpVerb = Parser.parseForHttpVerb(request);
         String pathUrl = Parser.parseForPathUrl(request);
         return httpVerb + " " + pathUrl;
-    }
-
-    private String actionForForm() throws IOException {
-        String action = httpVerb(getRoute());
-        FormController formController = new FormController();
-
-        if(action.equals("GET")) {
-            String response = formController.get();
-            return response;
-        } else if(action.equals("POST")) {
-            String response = formController.post(request);
-            return response;
-        } else if(action.equals("PUT")) {
-            //do the put action
-        } else if(action.equals("DELETE")) {
-            //do the delete action
-        }
-
-        return "";
     }
 
     private String httpVerb(String route) {
--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,6 +25,43 @@ public class Router {
 
         routesToReturn.put("/", rootRoutes);
         return routesToReturn;
+    }
+
+    public String chooseRoute(boolean pathExists, String route, String request) throws IOException {
+        String response = "";
+        String path = Parser.parseForPathUrl(route);
+        String httpVerb = Parser.parseForHttpVerb(route);
+
+        if(path.equals("/")) {
+            String responseHeader = "HTTP/1.1 200 OK\r\n\r\n";
+            InputStream fileStream = Server.class.getResourceAsStream("index.html");
+            String responseBody = Parser.parseInputStream(fileStream);
+            response = responseHeader + responseBody;
+        } else if(path.equals("/form")) {
+            FormController formController = new FormController();
+            response = chooseAndCallControllerAction(formController, httpVerb, request);
+        } else if(httpVerb.equals("POST") && path.equals("/form")) {
+            response = "HTTP/1.1 200 OK\r\n";
+        } else if(httpVerb.equals("OPTIONS") && path.equals("/")) {
+            response = "HTTP/1.1 200 OK\r\nAllow: GET,HEAD,POST,OPTIONS,PUT\r\n";
+        } else if(pathExists) {
+            response = "HTTP/1.1 405 Method now allowed";
+        } else {
+            response = "HTTP/1.1 404 Not Found\r\n";
+        }
+
+        return response;
+    }
+
+    private String chooseAndCallControllerAction(Controller controller, String httpVerb, String request) throws IOException {
+        String response =  "";
+        if(httpVerb.equals("GET")) {
+            response = controller.get();
+        } else if(httpVerb.equals("POST")) {
+            response = controller.post(request);
+        }
+
+        return response;
     }
 
 }

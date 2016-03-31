c8b32a9a96e9b96f4db6b3ee9b765caf9b24df3d
Robert Gu <robertgu@Roberts-MacBook-Pro.local>
2016-03-30 19:00:22 -0700

    Post request is handle and body is updated
    action for form is called when a /form url is requested, if a get
    request is called the response includes a 200 OK and the response body
    is simply a blank html page except an h1 stating the title.
    
    A post request finds the file, updates that file, then sends only the
    response header back.
    
    When a get request is called again the user sees the updated page with
    that data.

--- src/main/java/Handler.java
+++ src/main/java/Handler.java
@@ -1,9 +1,10 @@
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.URL;
 
 public class Handler {
     private String route;
     private Router router;
+    private String request;
 
     public Handler() {
         router = new Router();
@@ -18,19 +19,21 @@ public class Handler {
     }
 
     public String handleRequest(InputStream socketInputStream) throws IOException {
-        setRoute(parseForRoute(socketInputStream));
+        request = Parser.parseRequest(socketInputStream);
+        route = (parseForRoute());
         boolean pathExists = router.pathExists(Parser.parseForPathUrl(getRoute()));
         return handleResponse(pathExists);
     }
 
     private String handleResponse(boolean pathExists) throws IOException {
         String response = "";
-
         if(httpVerb(getRoute()).equals("GET") && path(getRoute()).equals("/")) {
             String responseHeader = "HTTP/1.1 200 OK\r\n\r\n";
             InputStream fileStream = Server.class.getResourceAsStream("index.html");
             String responseBody = Parser.parseInputStream(fileStream);
             response = responseHeader + responseBody;
+        } else if(path(getRoute()).equals("/form")) {
+            response = actionForForm();
         } else if(httpVerb(getRoute()).equals("POST") && path(getRoute()).equals("/form")) {
             response = "HTTP/1.1 200 OK\r\n";
         } else if(httpVerb(getRoute()).equals("OPTIONS") && path(getRoute()).equals("/")) {
@@ -44,11 +47,52 @@ public class Handler {
         return response;
     }
 
-    private String parseForRoute(InputStream socketInputStream) throws IOException {
-        String requestHeader = Parser.parseRequest(socketInputStream);
-        String httpVerb = Parser.parseForHttpVerb(requestHeader);
-        String pathUrl = Parser.parseForPathUrl(requestHeader);
+    private String parseForRoute() throws IOException {
+        String httpVerb = Parser.parseForHttpVerb(request);
+        String pathUrl = Parser.parseForPathUrl(request);
         return httpVerb + " " + pathUrl;
+    }
+
+    private String actionForForm() throws IOException {
+        String action = httpVerb(getRoute());
+
+        if(action.equals("GET")) {
+            String responseHeader = "HTTP/1.1 200 OK\r\n\r\n";
+            InputStream fileStream = Server.class.getResourceAsStream("form.html");
+            String responseBody = Parser.parseInputStream(fileStream);
+            return responseHeader + responseBody;
+        } else if(action.equals("POST")) {
+            String responseHeader = "HTTP/1.1 200 OK\r\n\r\n";
+
+            URL url = getClass().getResource("form.html");
+            File fileToUpdate = new File(url.getPath());
+            InputStream htmlInputStream = new FileInputStream(fileToUpdate);
+            String updatedHtml = updateHtml(htmlInputStream);
+            byte[] buffer = updatedHtml.getBytes();
+            InputStream fileStream = new ByteArrayInputStream(updatedHtml.getBytes());
+            FileOutputStream fileOutputStream = new FileOutputStream(new File(url.getPath()));
+            fileOutputStream.write(buffer);
+            fileOutputStream.close();
+
+            return responseHeader;
+        } else if(action.equals("PUT")) {
+            //do the put action
+        } else if(action.equals("DELETE")) {
+            //do the delete action
+        }
+
+        return "";
+    }
+
+    private String updateHtml(InputStream inputStream) throws IOException {
+        String htmlText = Parser.parseInputStream(inputStream);
+
+        String[] requestWords = request.split(" ");
+        String requestBody = requestWords[requestWords.length - 1];
+        String replacingText = "<p>" + requestBody;
+        String updatedHtmlText = htmlText.replace("<p>", replacingText);
+
+        return updatedHtmlText;
     }
 
     private String httpVerb(String route) {

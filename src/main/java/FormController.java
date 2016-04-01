a2e5a447d237d6d28ce321e249336abbdfedbbb5
Robert Gu <robertgu@Roberts-MacBook-Pro.local>
2016-04-01 10:18:35 -0700

    Post works again, all is right with the world
    Creating files and retrieving files using a relative path so they work
    if I build my project using Gradle.

--- src/main/java/FormController.java
+++ src/main/java/FormController.java
@@ -8,7 +8,7 @@ public class FormController extends Controller {
         String responseHeader = "HTTP/1.1 200 OK\r\n\r\n";
         String responseBody;
 
-        InputStream fileStream = Server.class.getResourceAsStream("form.txt");
+        InputStream fileStream = new FileInputStream("../resources/main/form.txt");
         responseBody = Parser.parseInputStream(fileStream);
 
         return responseHeader + responseBody;
@@ -16,18 +16,22 @@ public class FormController extends Controller {
 
     public String post(String request) throws IOException {
         String responseHeader = "HTTP/1.1 200 OK\r\n\r\n";
+        String textToWrite = updateText(request);
+        URL url = getClass().getResource("form.txt");
 
-        URL url = getClass().getResource("form.html");
-        File fileToUpdate = new File(url.getPath());
-        InputStream htmlInputStream = new FileInputStream(fileToUpdate);
-        String updatedHtml = updateHtml(htmlInputStream, request);
-
-        byte[] buffer = updatedHtml.getBytes();
-        InputStream fileStream = new ByteArrayInputStream(updatedHtml.getBytes());
-        FileOutputStream fileOutputStream = new FileOutputStream(new File(url.getPath()));
-
-        fileOutputStream.write(buffer);
-        fileOutputStream.close();
+        if (url != null) {
+            File fileToOverWrite = new File(url.getPath());
+            System.out.println(fileToOverWrite.canWrite());
+            FileWriter writer = new FileWriter(fileToOverWrite, false);
+            writer.write(textToWrite);
+            writer.close();
+        } else {
+            File newFile = new File("../resources/main/form.txt");
+            FileWriter writer = new FileWriter(newFile, false);
+            writer.write(textToWrite);
+            newFile.setWritable(true, false);
+            writer.close();
+        }
 
         return responseHeader;
     }
@@ -39,18 +43,14 @@ public class FormController extends Controller {
         File fileToDelete = new File(url.getPath());
         fileToDelete.delete();
 
+        //delete a file if it exists if not don't try and delete it just return a 200ok !
+
         return responseHeader;
     }
 
-    private String updateHtml(InputStream inputStream, String request) throws IOException {
-        String htmlText = Parser.parseInputStream(inputStream);
-
+    private String updateText(String request) throws IOException {
         String[] requestWords = request.split(" ");
-        String requestBody = requestWords[requestWords.length - 1];
-        String replacingText = "<p>" + requestBody;
-        String updatedHtmlText = htmlText.replace("<p>", replacingText);
-
-        return updatedHtmlText;
+        return requestWords[requestWords.length - 1];
     }
 
 }

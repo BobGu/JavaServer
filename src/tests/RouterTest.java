a5aafe9faa1103c33d9b2a94a8a02ca0868d0c6c
Robert Gu <robertgu@Roberts-MacBook-Pro.local>
2016-04-04 14:41:49 -0700

    Router can return the appropriate response for a post request

--- src/tests/RouterTest.java
+++ src/tests/RouterTest.java
@@ -9,12 +9,24 @@ import static org.junit.Assert.assertEquals;
 public class RouterTest {
 
     @Test
-    public void TestHandleReturnsAResponse() throws IOException {
+    public void TestHandleReturnsAppropriateResponse() throws IOException {
         Map<String, Controller> routes = new HashMap<String, Controller>();
         routes.put("/", new MockIndexController());
 
         Router router = new Router(routes);
         MockRequest request = new MockRequest("/", "GET", null);
+        String response = router.handle(request);
+
+        assertEquals("HTTP/1.1 200 OK\r\n\r\n", response);
+    }
+
+    @Test
+    public void TestHandleReturnsCorrectResponse() throws IOException {
+        Map<String, Controller> routes = new HashMap<String, Controller>();
+        routes.put("/", new MockIndexController());
+
+        Router router = new Router(routes);
+        MockRequest request = new MockRequest("/", "POST", "data=thisNewData");
         String response = router.handle(request);
 
         assertEquals("HTTP/1.1 200 OK\r\n\r\n", response);
@@ -43,7 +55,7 @@ public class RouterTest {
         }
 
         public String post(String request) {
-            return "yes";
+            return "HTTP/1.1 200 OK\r\n\r\n";
         }
 
         public String delete() {

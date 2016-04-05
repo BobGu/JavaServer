43981b41b7f77b4b00edaf94155a0b4f04329d5d
Robert Gu <robertgu@Roberts-MacBook-Pro.local>
2016-04-05 10:50:03 -0700

    Form controller deletes file if it no longer exists

--- src/tests/FormControllerTest.java
+++ src/tests/FormControllerTest.java
@@ -3,20 +3,33 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class FormControllerTest {
     private FormController formController;
+    private String path;
 
     @Before
     public void TestCreateFormController() {
         String path = "src/tests/TestFiles/fakeform.txt";
         formController = new FormController(path);
+    }
+
+    public void createFile() throws IOException {
+        File file = new File("src/tests/TestFiles/fakeform.txt");
+        file.createNewFile();
+
+        FileWriter writer = new FileWriter("src/tests/TestFiles/fakeform.txt", false);
+        writer.write("data=form form test");
+        writer.close();
     }
 
     @Test
@@ -35,5 +48,15 @@ public class FormControllerTest {
         Assert.assertThat(getResponse, containsString("data=hello"));
     }
 
+    @Test
+    public void TestDeleteMethodDeletesAFile() throws IOException {
+        createFile();
+        Assert.assertThat(formController.get(), containsString("data=form form test"));
 
+        String deleteResponse = formController.delete();
+        String getResponse = formController.get();
+
+        Assert.assertThat(deleteResponse, containsString("HTTP/1.1 200 OK\r\n\r\n"));
+        assertTrue(!getResponse.contains("data=form form test"));
+    }
 }

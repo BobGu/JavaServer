58f63cb46dd68103cf210bd967abfa7300ea71ef
Robert Gu <robertgu@Roberts-MacBook-Pro.local>
2016-04-14 13:52:42 -0700

    Log Controller added, logs a visit to the /log route and returns a 200 okay resopnse
    /log route added to routes in the router

---
+++ src/main/java/Controllers/LogController.java
@@ -0,0 +1,23 @@
+package Controllers;
+import Requests.Request;
+import httpStatus.HttpStatus;
+import logs.Log;
+import specialCharacters.EscapeCharacters;
+
+public class LogController implements Controller{
+
+    public String handle(Request request) {
+        String response = "";
+        if (request.getHttpVerb().equals("GET")) {
+            response = get();
+        }
+
+        return response;
+    }
+
+    private String get() {
+        Log log = Log.getInstance();
+        log.addVisit("GET /log HTTP/1.1");
+        return HttpStatus.okay + EscapeCharacters.newline + EscapeCharacters.newline;
+    }
+}
--- src/main/java/Routes/Router.java
+++ src/main/java/Routes/Router.java
@@ -41,6 +41,7 @@ public class Router {
         routes.add(new Route("/method_options",  new MethodOptionsController()));
         routes.add(new Route("/parameters", new ParameterController()));
         routes.add(new Route("/logs", new LogsController()));
+        routes.add(new Route("/log", new LogController()));
     }
 
     private Optional<Route> findRoute(String path) {

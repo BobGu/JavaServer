177b84daf6621a39d0a789215091b7d7fcf74725
Robert Gu <robertgu@Roberts-MacBook-Pro.local>
2016-04-26 15:01:00 -0700

    Refactored how a partial file content is found and returned as a response

--- src/main/java/controllers/FileController.java
+++ src/main/java/controllers/FileController.java
@@ -8,6 +8,8 @@ import specialCharacters.EscapeCharacters;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -78,16 +80,11 @@ public class FileController implements Controller {
         responseHeadersString += "Content-Length: " + responseBody.length + EscapeCharacters.newline + EscapeCharacters.newline;
         byte[] responseHeader = responseHeadersString.getBytes();
 
-        byte[] response = new byte[responseHeader.length + responseBody.length];
-        System.arraycopy(responseHeader, 0, response, 0, responseHeader.length);
-        System.arraycopy(responseBody, 0, response, responseHeader.length, responseBody.length);
-
+        byte[] response = addResponseHeaderAndBody(responseHeader, responseBody);
         return response;
     }
 
     private byte[] partialFileResponse(String fullRequest) throws IOException {
-        String responseHeaderString = "";
-        responseHeaderString += HttpStatus.partialContent + EscapeCharacters.newline;
 
         String[] linesOfRequest = fullRequest.split(EscapeCharacters.newline);
         String rangeRequest = linesOfRequest[1];
@@ -97,33 +94,53 @@ public class FileController implements Controller {
         String startOfRange = findRange(rangeRequest, startOfRangePattern);
         String endOfRange = findRange(rangeRequest, endOfRangePattern);
         byte[] fullContent = reader.read(fileLocation);
+
+
+        Map<String,String> rangeValues = findRange(startOfRange, endOfRange, fullContent.length);
+        int start = Integer.parseInt(rangeValues.get("start"));
+        int end = Integer.parseInt(rangeValues.get("end"));
+        byte[] responseBody = Arrays.copyOfRange(fullContent, start, end);
+        byte[] responseHeader = partialResponseHeader(start, end, fullContent.length);
+
+        byte[] response = addResponseHeaderAndBody(responseHeader, responseBody);
+        return response;
+    }
+
+    private byte[] addResponseHeaderAndBody(byte[] responseHeader, byte[] responseBody) {
+        byte[] response = new byte[responseHeader.length + responseBody.length];
+        System.arraycopy(responseHeader, 0, response, 0, responseHeader.length);
+        System.arraycopy(responseBody, 0, response, responseHeader.length, responseBody.length);
+        return response;
+    }
+
+    private byte[] partialResponseHeader(int start, int end, int fileLength) {
+        String responseHeaderString = "";
+        responseHeaderString += HttpStatus.partialContent + EscapeCharacters.newline;
+        responseHeaderString += "Content-Range: bytes " + String.valueOf(start) + "-" + String.valueOf(end - 1) + "/" + fileLength + EscapeCharacters.newline;
+        responseHeaderString += "Content-Length: " + String.valueOf(end - start) + EscapeCharacters.newline;
+        responseHeaderString += "Content-Type: " + determineContentType(fileLocation) + EscapeCharacters.newline + EscapeCharacters.newline;
+        return responseHeaderString.getBytes();
+    }
+
+    private Map<String,String> findRange(String startOfRange, String endOfRange, int lengthOfFileContent) {
+        Map<String,String> rangeValues = new HashMap<String,String>();
         int start;
         int end;
 
         if (startOfRange.equals("")) {
-            start = fullContent.length - Integer.parseInt(endOfRange);
-            end = fullContent.length;
+            start = lengthOfFileContent - Integer.parseInt(endOfRange);
+            end = lengthOfFileContent;
         } else if(endOfRange.equals("")) {
             start = Integer.parseInt(startOfRange);
-            end = fullContent.length;
+            end = lengthOfFileContent;
         } else {
             start = Integer.parseInt(startOfRange);
             end = Integer.parseInt(endOfRange) + 1;
         }
+        rangeValues.put("start", String.valueOf(start));
+        rangeValues.put("end", String.valueOf(end));
 
-        byte[] responseBody = Arrays.copyOfRange(fullContent, start, end);
-        end = end - 1;
-
-        responseHeaderString += "Content-Range: bytes " + String.valueOf(start) + "-" + String.valueOf(end) + "/" + fullContent.length + EscapeCharacters.newline;
-        responseHeaderString += "Content-Length: " + String.valueOf(end - start + 1) + EscapeCharacters.newline;
-        responseHeaderString += "Content-Type: " + determineContentType(fileLocation) + EscapeCharacters.newline + EscapeCharacters.newline;
-
-        byte[] responseHeader = responseHeaderString.getBytes();
-        byte[] response = new byte[responseHeader.length + responseBody.length];
-        System.arraycopy(responseHeader, 0, response, 0, responseHeader.length);
-        System.arraycopy(responseBody, 0, response, responseHeader.length, responseBody.length);
-
-        return response;
+        return rangeValues;
     }
 
     private String findRange(String rangeRequest, Pattern pattern) {
